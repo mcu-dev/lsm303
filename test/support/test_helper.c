0cb5faf759367017e44abb94d036bef430d53570
aeraterta <aeraterta@gmail.com>
2025-01-14 02:27:25 +0800

    Transfer functions and updated unit-test fixtures

---
+++ test/support/test_helper.c
@@ -0,0 +1,21 @@
+#include "test_helper.h"
+#include "unity.h"
+
+static uint8_t *expected_data    = NULL;
+static size_t expected_data_size = 0;
+
+void set_expected_i2c_write_data(uint8_t *expected_data_param, size_t size) {
+  expected_data      = expected_data_param;
+  expected_data_size = size;
+}
+
+signed char i2c_write_bytes_callback(uint8_t address, uint8_t *data_buffer,
+                                     int cmock_num_calls) {
+  // Validate the address
+  TEST_ASSERT_EQUAL(ACC_I2C_ADDRESS, address);
+
+  // Validate the data buffer
+  TEST_ASSERT_EQUAL_UINT8_ARRAY(expected_data, data_buffer, expected_data_size);
+
+  return LSM303_STATUS_SUCCESS;
+}
